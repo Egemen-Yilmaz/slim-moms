91f8fd85d401996b60304b2ce765db30d5da9a1e
hatice_v_daalen <HaticevanD@users.noreply.github.com>
2026-05-21 11:55:07 +0200

    feat(diary): initialize diary page with base components

--- frontend/src/pages/DiaryPage/DiaryPage.jsx
+++ frontend/src/pages/DiaryPage/DiaryPage.jsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+
+import DiaryDateCalendar from "../../components/diary/DiaryDateCalendar/DiaryDateCalendar";
+import DiaryAddProductForm from "../../components/forms/DiaryAddProductForm/DiaryAddProductForm";
+import DiaryProductsList from "../../components/diary/DiaryProductsList/DiaryProductsList";
+
+import css from "./DiaryPage.module.css";
+
 export default function DiaryPage() {
-  return <div>Diary Page</div>;
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const [refresh, setRefresh] = useState(false);
+
+  const triggerRefresh = () => {
+    setRefresh((prev) => !prev);
+  };
+
+  return (
+    <div className={css.page}>
+      {/* DATE SELECTOR */}
+      <DiaryDateCalendar
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+      />
+
+      {/* ADD PRODUCT */}
+      <DiaryAddProductForm
+        selectedDate={selectedDate}
+        onSuccess={triggerRefresh}
+      />
+
+      {/* LIST */}
+      <DiaryProductsList
+        selectedDate={selectedDate}
+        refresh={refresh}
+        onChange={triggerRefresh}
+      />
+    </div>
+  );
 }
